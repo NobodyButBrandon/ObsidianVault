db1011ea879c61071321157049b89400bc7ad64e
Brandon Trigueros <brandon.trigueros@ucr.ac.cr>
2025-05-18 21:03:09 -0600

    Auto commit 2025-05-18 21:03:09

--- obsidianopen.sh
+++ obsidianopen.sh
@@ -1,4 +1,3 @@
 cd "/c/Users/Brandon/Documents/ObsidianVault" || { echo "Directorio no encontrado"; exit 1; }
 git pull;
-start "" "C:\Users\Brandon\AppData\Local\Programs\Obsidian\Obsidian.exe"
 exit
